84a3e7751554440f3c54265ae9ad26d707ab5c53
zk <zk@123.com>
2023-07-10 17:59:44 +0800

    Initial commit

---
+++ src/com/zk/Test01.java
@@ -0,0 +1,21 @@
+package com.zk;
+
+/**
+ * @Author: zk
+ * @Date: 2023/07/08/10:14
+ * @Description:
+ */
+public class Test01 {
+    public static void main(String[] args) {
+        System.out.println("gender : 变量 ");
+        //一个字节（byte）等于  8位（bit）  byte1  short2  int4  long8
+
+
+        byte s = 125;
+        System.out.println("shuchuing"+s);
+        double d = 123.1;
+        float f = 123.1f;
+        System.out.println(d+f);
+
+    }
+}
